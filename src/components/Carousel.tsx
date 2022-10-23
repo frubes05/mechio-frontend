e6e54f04fe38f369fc7e2fb140292b85b2b431da
Filip Rubes <filip.rubes2@gmail.com>
2022-10-23 22:49:07 +0200

    main changed how carousel items look on home page

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -5,8 +5,11 @@ import { Container, Row, Col } from "react-bootstrap";
 import { IJobs } from "../pages/jobs/Jobs.types";
 import useFetch from "../hooks/useFetch";
 import { carouselConfig } from "./carousel.config";
+import moment from "moment";
+import "moment/locale/hr";
 
 const Carousel = () => {
+  moment().locale("hr");
   const [lastAdded, setLastAdded] = useState<IJobs[] | null>(null);
 
   const getJobs = useFetch({
@@ -26,18 +29,25 @@ const Carousel = () => {
             {lastAdded.map((job) => (
               <SplideSlide key={job._id}>
                 <Link to={`/poslovi/${job._id}`}>
-                  <article className="home__jobs-article">
-                    <div className="home__jobs-overlay">
+                  <article className="jobs__card">
+                    <div className="jobs__card-picture">
                       <img
-                        className="home__jobs-image"
+                        className="jobs__card-image"
                         src={
                           "https://mechio-api-test.onrender.com/" +
                           job.companyImage
                         }
                       />
+                      <span className="jobs__card-pill">
+                        {moment(job.date.toString()).startOf("day").fromNow()}
+                      </span>
                     </div>
-                    <div className="home__jobs-text">
-                      <h3 className="home__jobs-position">{job.position}</h3>
+                    <div className="jobs__card-content">
+                      <ul className="jobs__card-list">
+                        <li className="jobs__card-item">{job.company}, </li>
+                        <li className="jobs__card-item">{job.location}</li>
+                      </ul>
+                      <h2 className="jobs__card-title">{job.position}</h2>
                     </div>
                   </article>
                 </Link>
