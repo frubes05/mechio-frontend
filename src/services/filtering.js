3d05ae2091a26f89624c91949115a263daf1293e
Filip Rubes <filip.rubes2@gmail.com>
2022-10-23 22:19:47 +0200

    main added main filtering logic

--- src/services/filtering.js
+++ src/services/filtering.js
@@ -8,12 +8,23 @@ export const filteringService = (options, list) => {
         const selectedItems = list.filter(item => item[type] === value);
         chosenElements.push(...selectedItems);
     })
-    let chosenElementsWithRelations = [];
-    /*options.forEach(option => {
-        const {type, value} = option;
-        const selectedItems = chosenElements.filter(item => item[type] === value);
-        chosenElementsWithRelations.push(...selectedItems);
-    })*/
     chosenElements = Array.from(new Set([...chosenElements]));
+    const changed = options.map(opt => {
+        return {
+            [`${opt.type}`]: opt.value
+        }
+    })
+    const filteringObject = Object.assign(...changed);
+    chosenElements = chosenElements.map(elem => {
+        const togetherParts = [];
+        for(let key in filteringObject) {
+            if (elem[key] === filteringObject[key]) {
+                togetherParts.push(true);
+            }
+        }
+        if (togetherParts.length === Object.keys(filteringObject).length) {
+            return elem;
+        }
+    }).filter(elem => elem !== undefined);
     return chosenElements;
 }
